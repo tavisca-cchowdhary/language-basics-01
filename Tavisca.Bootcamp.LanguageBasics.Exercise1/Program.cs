adb4a4eed72a5da49f03310b7214417e2946d1fc
Chaman Chowdhary <cchowdhary@tavisca.in>
2019-06-22 20:57:17 +0530

    Program completed

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -22,8 +22,56 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 
         public static int FindDigit(string equation)
         {
-            // Add your code here.
+            String[] value=equation.Split('*','=');
+            int first=0,second=0,third=0;
+            if (value[0].Contains("?"))
+            {
+                second = Convert.ToInt32(value[1]);
+                third = Convert.ToInt32(value[2]);
+
+                if(third%second == 0)
+                    return Findmiss(value[0],third/second);
+                return -1;
+            }
+            else if (value[1].Contains("?"))
+            {
+                first = Convert.ToInt32(value[0]);
+                third = Convert.ToInt32(value[2]);
+
+                if(third%first == 0)
+                    return Findmiss(value[1],third/first);;
+                return -1;
+            }
+            else
+            {
+                second = Convert.ToInt32(value[1]);
+                first = Convert.ToInt32(value[0]);
+                return Findmiss(value[2],first*second);
+            }
+            //foreach(String a in value)
+              //  Console.WriteLine(a);
+            //return 1;
             throw new NotImplementedException();
+        }
+
+        public static int Findmiss(string val,int temp)
+        {
+            Console.WriteLine(temp);
+            int position = val.IndexOf(@"?");
+            position=val.Length-position-1;
+            int poscount=0;
+            Console.WriteLine(position);
+            while(temp>0)
+            {
+                if(position==poscount)
+                {
+                    Console.WriteLine(temp%10);
+                    return temp%10;
+                }
+                poscount +=1;
+                temp /= 10;
+            }
+            return -1;
         }
     }
 }
